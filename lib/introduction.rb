f1cf3af492e6ac2b50d0913606a35edc66fa59d3
Garrett Hill <ghill012@gmail.com>
2020-07-02 17:03:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,13 +8,13 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language(Garrett, Ruby)
+introduction_with_language(Bob, Ruby)
 
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional(Garrett)
+introduction_with_language_optional(Tim)
 
 
 
